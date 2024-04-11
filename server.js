fa951057d24025c9383f5c9608afdb9b87ae8a0d
escobarrro <pblplrz@gmail.com>
2024-04-11 12:12:50 +0200

    body parser add

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const cors = require('cors');
 const knex = require('knex');
+const bodyParser = require('body-parser');
 
 
 const db = knex({
